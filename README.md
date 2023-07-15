6eae6dfbc284a27dff5f7aff06524853490f7db9
nora <usernamesaresoharddtomake@gmail.com>
2023-07-15 23:30:07 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple tic-tac-toe game for 2 players.
---
+++ script.js
@@ -0,0 +1,10 @@
+// board module (iife)
+const gameBoard = (() => {
+  let board = ["", "", "", "", "", "", "", "", ""]
+
+  const render = ()
+})();
+
+
+
+// player factory
